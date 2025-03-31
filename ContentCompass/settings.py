af581671fdc54a009cf9b56b3b13e02052879316
IslamKolade <islamsanni66@gmail.com>
2025-03-31 19:52:22 +0100

    Set Debug to False

--- ContentCompass/settings.py
+++ ContentCompass/settings.py
@@ -8,8 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-#DEBUG = config('DEBUG', cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
+#DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
